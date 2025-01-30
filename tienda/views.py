5bec982bb52bf94a239cdead4b3236ae317da305
Miguel Abril <miguel.abril.alamo.al@iespoligonosur.org>
2025-01-30 08:54:50 +0100

    pongo las urls de pythonanywhere

--- tienda/views.py
+++ tienda/views.py
@@ -17,27 +17,27 @@ def index(request):
 def productos_listar_api(request):
     #El :9090 es el puerto que he usado
     headers = {'Authorization':'Bearer '+env('ADMIN')}
-    response = requests.get('http://127.0.0.1:9090/api/v1/productos',headers=headers)
+    response = requests.get('https://mabrala.pythonanywhere.com/api/v1/productos',headers=headers)
     productos = response.json()
     return render(request, 'productos/lista_basica.html',{'productos':productos})
 
 def productos_listar_mejorado_api(request):
     #Usando el token del admin
     headers = {'Authorization':'Bearer '+env('ADMIN')}
-    response = requests.get('http://127.0.0.1:9090/api/v1/productos-mejorado',headers=headers)
+    response = requests.get('https://mabrala.pythonanywhere.com/api/v1/productos-mejorado',headers=headers)
     productos = response.json()
     return render(request, 'productos/lista.html',{'productos':productos})
 
 def calzado_listar(request):
     #Usando token para un cliente
     headers = {'Authorization':'Bearer '+env("CLIENTE")}
-    response = requests.get('http://127.0.0.1:9090/api/v1/calzados',headers=headers)
+    response = requests.get('https://mabrala.pythonanywhere.com/api/v1/calzados',headers=headers)
     calzados = response.json()
     return render(request, 'calzados/lista.html',{'calzados':calzados})
 
 def consolas_listar(request):
     #Usando token para un vendedor
     headers = {'Authorization':'Bearer '+env('VENDEDOR')}
-    response = requests.get('http://127.0.0.1:9090/api/v1/consolas',headers=headers)
+    response = requests.get('https://mabrala.pythonanywhere.com/api/v1/consolas',headers=headers)
     consolas = response.json()
     return render(request, 'consolas/lista.html',{'consolas':consolas})
