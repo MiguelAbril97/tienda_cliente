a3b9b2afe5ec6e374bd4130ead4c38e7f4aa04c8
Miguel Abril <miguel.abril.alamo.al@iespoligonosur.org>
2025-01-31 13:43:33 +0100

    refactorizo codigo

--- tienda/views.py
+++ tienda/views.py
@@ -14,30 +14,36 @@ env = environ.Env()
 def index(request):
     return render(request,'index.html')
 
+def peticion_v1(direccion):
+    return 'https://mabrala.pythonanywhere.com/api/v1/'+direccion
+
+def respuesta (objeto):
+    return objeto.json()
+
 def productos_listar_api(request):
     #El :9090 es el puerto que he usado
     headers = {'Authorization':'Bearer '+env('ADMIN')}
-    response = requests.get('https://mabrala.pythonanywhere.com/api/v1/productos',headers=headers)
-    productos = response.json()
+    response = requests.get(peticion_v1('productos'),headers=headers)
+    productos = respuesta(response)
     return render(request, 'productos/lista_basica.html',{'productos':productos})
 
 def productos_listar_mejorado_api(request):
     #Usando el token del admin
     headers = {'Authorization':'Bearer '+env('ADMIN')}
-    response = requests.get('https://mabrala.pythonanywhere.com/api/v1/productos-mejorado',headers=headers)
-    productos = response.json()
+    response = requests.get(peticion_v1('productos-mejorado'),headers=headers)
+    productos = respuesta(response)
     return render(request, 'productos/lista.html',{'productos':productos})
 
 def calzado_listar(request):
     #Usando token para un cliente
     headers = {'Authorization':'Bearer '+env("CLIENTE")}
-    response = requests.get('https://mabrala.pythonanywhere.com/api/v1/calzados',headers=headers)
-    calzados = response.json()
+    response = requests.get(peticion_v1('calzados'),headers=headers)
+    calzados = respuesta(response)
     return render(request, 'calzados/lista.html',{'calzados':calzados})
 
 def consolas_listar(request):
     #Usando token para un vendedor
     headers = {'Authorization':'Bearer '+env('VENDEDOR')}
-    response = requests.get('https://mabrala.pythonanywhere.com/api/v1/consolas',headers=headers)
-    consolas = response.json()
+    response = requests.get(peticion_v1('consolas'),headers=headers)
+    consolas = respuesta(response)
     return render(request, 'consolas/lista.html',{'consolas':consolas})
