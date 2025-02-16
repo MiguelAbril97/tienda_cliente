87c9d011c9b4e59ef23eeaf347c45ff516d93274
Miguel Abril <miguel.abril.alamo.al@iespoligonosur.org>
2025-02-16 17:24:40 +0100

    views url y templates hechas

--- tienda/views.py
+++ tienda/views.py
@@ -171,7 +171,7 @@ def producto_editar(request, producto_id):
         datos['fecha_de_publicacion'] = fecha_aware.strftime('%Y-%m-%d %H:%M:%S')
         
         response = requests.put(
-                                peticion_v1('productos/actualizar/'+str(producto_id)),
+                                peticion_v1('productos/editar/'+str(producto_id)),
                                 headers=crear_cabecera(),
                                 data=json.dumps(datos)
                                 )
@@ -188,7 +188,49 @@ def producto_editar(request, producto_id):
             else:
                 return mi_error_500(request)
     return render(request, 'productos/crear.html', {"formulario":formulario})
+
+def producto_actualizar_nombre(request, producto_id):
+    
+    datosFormulario = None
+    
+    if request.method == "POST":
+        datosFormulario = request.POST
         
+        producto = helper.obtener_producto(producto_id)
+        formulario = ProductoActualizarNombreForm(datosFormulario,
+                                                  initial={'nombre': producto['nombre']})
+        
+    if (request.method == 'POST'):
+        formulario = ProductoActualizarNombreForm(request.POST)
+        if formulario.is_valid():
+            headers = crear_cabecera()
+            response = requests.put(peticion_v1('productos/actualizar/'+str(producto_id)), 
+                                    headers=headers, 
+                                    data=json.dumps(formulario.data))
+            if response.status_code == requests.codes.ok:
+                return redirect("productos_listar")
+            else:
+                print(response.status_code)
+                response.raise_for_status()
+        else:
+            print(formulario.errors)
+    return render(request, 'productos/actualizar_nombre.html', {"formulario":formulario})
+
+def producto_eliminar(request, producto_id):
+    try:
+        headers = crear_cabecera()
+        response = requests.delete(peticion_v1('productos/eliminar/'+str(producto_id)), 
+                                headers=headers)
+        if( response.status_code == requests.codes.ok):
+            return redirect("productos_listar")
+        else:
+            print(response.status_code)
+            response.raise_for_status()
+    except Exception as err:
+        print(f'Ocurrió un error: {err}')
+        return mi_error_500(request)
+    return redirect("productos_listar")
+            
 
 #CRUD ManyToMany con tabla intermedia
 def compra_crear(request):
@@ -232,6 +274,95 @@ def compra_crear(request):
         formulario = CompraForm(None)
     return render(request, 'compras/crear.html', {"formulario":formulario})
 
+def compra_editar(request, compra_id):
+    
+    datosFormulario = None
+    
+    if request.method == "POST":
+        datosFormulario = request.POST
+    
+    compra = helper.obtener_compra(compra_id)
+    formulario = CompraForm(datosFormulario,
+                            initial={
+                                'fecha_compra': datetime.datetime.strptime(
+                                    compra['fecha_compra'], '%Y-%m-%d %H:%M:%S'),
+                                'total': compra['total'],
+                                'garantia': compra['garantia'],
+                                'comprador': compra['comprador']['id'],
+                                'producto': [producto['id'] for producto in compra['producto']]
+                            }
+                        )
+    if request.method == 'POST':
+        formulario = CompraForm(request.POST)
+        datos = formulario.data.copy()
+        
+        fecha_copia = request.POST['fecha_compra']
+        fecha = datetime.strptime(fecha_copia, '%Y-%m-%dT%H:%M')
+        fecha_aware = timezone.make_aware(fecha)
+        datos['fecha_compra'] = fecha_aware.strftime('%Y-%m-%d %H:%M:%S')
+        
+        response = requests.put(
+                                peticion_v1('compras/editar/'+str(compra_id)),
+                                headers=crear_cabecera(),
+                                data=json.dumps(datos)
+                                )
+        if(response.status_code == requests.codes.ok):
+            return redirect("productos_listar")
+        else:
+            if(response.status_code == 400):
+                errores = response.json()
+                for error in errores:
+                    formulario.add_error(error,errores[error])
+                return render(request, 
+                            'compras/crear.html',
+                            {"formulario":formulario})
+            else:
+                return mi_error_500(request)
+    return render(request, 'compras/crear.html', {"formulario":formulario})
+
+def compra_actualizar_garantia(request, compra_id):
+    
+    datosFormulario = None
+    
+    if request.method == "POST":
+        datosFormulario = request.POST
+        
+        compra = helper.obtener_compra(compra_id)
+        formulario = CompraActualizarGarantiaForm(datosFormulario,
+                                                 initial={'garantia': compra['garantia']})
+
+    if (request.method == 'POST'):
+        formulario = CompraActualizarGarantiaForm(request.POST)
+        if formulario.is_valid():
+            headers = crear_cabecera()
+            response = requests.put(peticion_v1('compras/actualizar/'+str(compra_id)), 
+                                    headers=headers, 
+                                    data=json.dumps(formulario.data))
+            if response.status_code == requests.codes.ok:
+                return redirect("productos_listar")
+            else:
+                print(response.status_code)
+                response.raise_for_status()
+        else:
+            print(formulario.errors)
+    return render(request, 'compras/actualizar_garantia.html', {"formulario":formulario})
+
+
+def compra_eliminar(request, compra_id):
+    try:
+        headers = crear_cabecera()
+        response = requests.delete(peticion_v1('compras/eliminar/'+str(compra_id)), 
+                                headers=headers)
+        if( response.status_code == requests.codes.ok):
+            return redirect("productos_listar")
+        else:
+            print(response.status_code)
+            response.raise_for_status()
+    except Exception as err:
+        print(f'Ocurrió un error: {err}')
+        return mi_error_500(request)
+    return redirect("productos_listar")
+   
 #CRUD ManyToOne
 def valoracion_crear(request):
     if(request.method == 'POST'):
@@ -263,6 +394,101 @@ def valoracion_crear(request):
             print(f'Ocurrió un error: {err}')
             return mi_error_500(request)
 
+def valoracion_editar(request, valoracion_id):
+    
+    datosFormulario = None
+    
+    if request.method == "POST":
+        datosFormulario = request.POST
+
+        valoracion = helper.obtener_valoracion(valoracion_id)
+        formulario = ValoracionForm(datosFormulario,
+                                    initial={
+                                        'puntuacion': valoracion['puntuacion'],
+                                        'comentario': valoracion['comentario'],
+                                        'fecha_valoracion': datetime.datetime.strptime(
+                                            valoracion['fecha_valoracion'], '%Y-%m-%d %H:%M:%S'),
+                                        'usuario': valoracion['usuario']['id'],
+                                        'compra': valoracion['compra']['id']
+                                    }
+                                )
+    if request.method == 'POST':
+        formulario = ValoracionForm(request.POST)
+        datos = formulario.data.copy()
+        
+        fecha_copia = request.POST['fecha_valoracion']
+        fecha = datetime.strptime(fecha_copia, '%Y-%m-%dT%H:%M')
+        fecha_aware = timezone.make_aware(fecha)
+        datos['fecha_valoracion'] = fecha_aware.strftime('%Y-%m-%d %H:%M:%S')
+        
+        response = requests.put(
+                                peticion_v1('valoraciones/editar/'+str(valoracion_id)),
+                                headers=crear_cabecera(),
+                                data=json.dumps(datos)
+                                )
+        if(response.status_code == requests.codes.ok):
+            return redirect("productos_listar")
+        else:
+            if(response.status_code == 400):
+                errores = response.json()
+                for error in errores:
+                    formulario.add_error(error,errores[error])
+                return render(request, 
+                            'valoraciones/crear.html',
+                            {"formulario":formulario})
+            else:
+                return mi_error_500(request)
+    return render(request, 'valoraciones/crear.html', {"formulario":formulario})
+
+
+def valoracion_actualizar_puntuacion(request, valoracion_id):
+    
+    datosFormulario = None
+    
+    if request.method == "POST":
+        datosFormulario = request.POST
+        
+        valoracion = helper.obtener_valoracion(valoracion_id)
+        formulario = ValoracionActualizarPuntuacionForm(datosFormulario,
+                                                       initial={'puntuacion': valoracion['puntuacion']})
+
+    if (request.method == 'POST'):
+        formulario = ValoracionActualizarPuntuacionForm(request.POST)
+        if formulario.is_valid():
+            headers = crear_cabecera()
+            response = requests.put(peticion_v1('valoraciones/actualizar/'+str(valoracion_id)), 
+                                    headers=headers, 
+                                    data=json.dumps(formulario.data))
+            if response.status_code == requests.codes.ok:
+                return redirect("productos_listar")
+            else:
+                print(response.status_code)
+                response.raise_for_status()
+        else:
+            print(formulario.errors)
+    return render(request, 'valoraciones/actualizar_puntuacion.html', {"formulario":formulario})
+
+def valoracion_eliminar(request, valoracion_id):
+    try:
+        headers = crear_cabecera()
+        response = requests.delete(peticion_v1('valoraciones/eliminar/'+str(valoracion_id)), 
+                                headers=headers)
+        if(response.status_code == requests.codes.ok):
+            return redirect("productos_listar")
+        else:
+            print(response.status_code)
+            response.raise_for_status()
+    except Exception as err:
+        print(f'Ocurrió un error: {err}')
+        return mi_error_500(request)
+    return redirect("productos_listar")
+
+################################################
+################################################
+##########  OTRAS VIEWS  #######################
+
+################################################
+################################################
          
 def calzado_listar(request):
     headers = crear_cabecera()
